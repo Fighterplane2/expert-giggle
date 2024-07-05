bd04330dea20d1938ec2f9cde6d6094c06e64090
Jaeden Waller <146506880+Fighterplane2@users.noreply.github.com>
2024-07-05 17:14:55 +0000

    js work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
-});
+//          //Below is an array creation and display code
+// let array = [];
+
+// for(let i = 0; i < 10; i++){
+//     array.push(i);
+// }
+
+// console.log(array);
+
+// for(let i = 0; i < 10; i++){
+//     window.alert(i);
+// }
+
+
+
+//          //Below is a name prompting and greeting code
+// let nameInput = prompt("What is your name?")
+// function greeting(name){
+//     window.alert("Hello " + name + " today is Tuesday.")
+// }
+// greeting(nameInput);
+
